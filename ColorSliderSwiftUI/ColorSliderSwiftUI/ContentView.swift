09ad5f31e7753094f60013073e382c5f7f2b694f
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-04-05 01:19:21 +0300

    project changed#1

--- ColorSliderSwiftUI/ColorSliderSwiftUI/ContentView.swift
+++ ColorSliderSwiftUI/ColorSliderSwiftUI/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+        @State private var red = Double.random(in: 0...255)
+        @State private var green = Double.random(in: 0...255)
+        @State private var blue = Double.random(in: 0...255)
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.lightGray)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 50) {
+                ColorView(red: red, green: green, blue: blue)
+            }
+        }
     }
 }
 
