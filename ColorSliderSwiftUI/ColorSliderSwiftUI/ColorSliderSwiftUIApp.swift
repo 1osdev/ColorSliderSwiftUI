5d3665a039b58eda4689ed2c832113a625d2c5ca
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-04-04 20:10:07 +0300

    project created

---
+++ ColorSliderSwiftUI/ColorSliderSwiftUI/ColorSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderSwiftUIApp.swift
+//  ColorSliderSwiftUI
+//
+//  Created by Дмитрий Смирнов on 4.04.22.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorSliderSwiftUI/ColorSliderSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorSliderSwiftUI
+//
+//  Created by Дмитрий Смирнов on 4.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
